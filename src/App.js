ee331e9fe09687f65d6dff2b2f07de1af4f33080
Sasha Rocha <sasharocha@desktop.local>
2022-07-21 19:02:58 -0300

    EcommerceRocha

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+
+import Nav from './components/NavBar.js';
+import './NavBar.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+     <Nav /> 
+    <h2>Bienvenido a mi Ecommerce</h2>
     </div>
+   
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+
+function Nav () {
+return (
+<nav className="nav">
+    <a href="#start">
+       <img src="/img/logoatom.png" alt="logo atom" className="nav_logo"></img>
+    </a>
+    <ul className="nav_menu">
+      <li>
+        <a href="logo" alt="logo whislist">  
+           <img src="/img/whislisticon.png" alt="logo whislist" className="nav_whislist"></img>
+        </a> 
+        </li>
+      <li className="nav__item">
+        <a href="#home" className="nav_link">
+            Home
+        </a>
+      </li>
+      <li className="nav__item">
+        <a href="#about" className="nav_link">
+            About
+        </a>
+      </li>
+      <li className="nav__item">
+        <a href="#users" className="nav_link">
+            Users
+        </a>
+      </li>
+    </ul>
+</nav>
+)}
+
+export default Nav;
